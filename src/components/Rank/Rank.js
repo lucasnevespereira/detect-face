67d407c387c83069e74cac2fe627e99b2320de0c
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-10-31 00:46:11 +0100

    init project & components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="white f3">
+        {"Lucas, you have detected faces in #5 pictures"}
+      </div>
+    </div>
+  );
+};
+
+export default Rank;
