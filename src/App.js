c3310a75f72bd9fb4dfbbd0a995ddc0c38c42ff4
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-01 12:15:03 +0100

    added fetch event on component did mount

--- src/App.js
+++ src/App.js
@@ -35,6 +35,12 @@ class App extends Component {
     isSignedIn: false,
   };
 
+  componentDidMount() {
+    fetch("http://localhost:5000")
+      .then((response) => response.json())
+      .then(console.log);
+  }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace =
       data.outputs[0].data.regions[0].region_info.bounding_box;
