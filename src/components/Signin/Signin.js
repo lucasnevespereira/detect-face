6e872ed50a5db55d7e64102a7a85d9c9ad0c766f
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-01 12:59:41 +0100

    refactored signin comp into a class comp with state to handle form

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,49 +1,69 @@
 import React from "react";
 
-const Signin = ({ onRouteChange }) => {
-  return (
-    <main className="signin pa4 black-80 br3 shadow-3 w-50 center">
-      <form className="measure center">
-        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-          <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-          <div className="mt3">
-            <label className="db fw6 lh-copy f6">Email</label>
-            <input
-              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-              type="email"
-              name="email-address"
-              id="email-address"
-            />
-          </div>
-          <div className="mv3">
-            <label className="db fw6 lh-copy f6">Password</label>
-            <input
-              className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-              type="password"
-              name="password"
-              id="password"
-            />
-          </div>
-          <div className="">
-            <input
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-              onClick={() => onRouteChange("home")}
-              type="submit"
-              value="Sign in"
-            />
-          </div>
-          <div className="lh-copy mt3">
-            <a
-              onClick={() => onRouteChange("register")}
-              className="f6 link dim black db pointer"
-            >
-              Register
-            </a>
-          </div>
-        </fieldset>
-      </form>
-    </main>
-  );
-};
+class Signin extends React.Component {
+  state = { signInEmail: "", signInPassword: "" };
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  onSubmitSignIn = () => {
+    console.log(JSON.stringify(this.state));
+    this.props.onRouteChange("home");
+  };
+
+  render() {
+    const { onRouteChange } = this.props;
+    return (
+      <main className="signin pa4 black-80 br3 shadow-3 w-50 center">
+        <form className="measure center">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6">Email</label>
+              <input
+                onChange={this.onEmailChange}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6">Password</label>
+              <input
+                onChange={this.onPasswordChange}
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </div>
+            <div className="">
+              <input
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                onClick={this.onSubmitSignIn}
+                type="submit"
+                value="Sign in"
+              />
+            </div>
+            <div className="lh-copy mt3">
+              <a
+                onClick={() => onRouteChange("register")}
+                className="f6 link dim black db pointer"
+              >
+                Register
+              </a>
+            </div>
+          </fieldset>
+        </form>
+      </main>
+    );
+  }
+}
 
 export default Signin;
