200a37906602cc158b7c571f7f7458bd274e35f6
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-01 14:56:22 +0100

    updated rank entries

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,8 +4,9 @@ const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className="center white f3">
-        {`Hello ${name}, your current entry count is #${entries} ..`}
+        {`Hello ${name}, your current entry count is... `}
       </div>
+      <div className="white f1">{entries}</div>
     </div>
   );
 };
