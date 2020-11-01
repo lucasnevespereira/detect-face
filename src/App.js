da4aba084277e6b9662413b349e4cafaf7371553
lucasnevespereira <pereiraneveslucas@gmail.com>
2020-11-01 19:36:35 +0100

    removed clarifai from app to be handled by the backend

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
 import { Component } from "react";
-
-const app = new Clarifai.App({
-  apiKey: "a0a359df4388405cb8979754587f9883",
-});
 
 const particlesOptions = {
   particles: {
@@ -78,8 +73,12 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch("http://localhost:5000/imageUrl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ input: this.state.input }),
+    })
+      .then((response) => response.json())
       .then((response) => {
         if (response) {
           fetch("http://localhost:5000/image", {
